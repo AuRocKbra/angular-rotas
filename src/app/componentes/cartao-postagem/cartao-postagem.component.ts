42110ad11ee922f77ff7d135960b8170918d5a01
augusto.espindola <augusto.espindola@sesicni.com.br>
2025-09-30 13:16:36 -0300

    Ajuste no projeto base e criação de rotas
    Ajusta o projeto base incluindo as vairáveis de input no componentes necessários e criando as rotas para as páginas de login e posts
    
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts
    	modified:   src/app/app.routes.ts
    	modified:   src/app/componentes/acoes-postagem/acoes-postagem.component.html
    	modified:   src/app/componentes/acoes-postagem/acoes-postagem.component.ts
    	modified:   src/app/componentes/cartao-postagem/cartao-postagem.component.html
    	modified:   src/app/componentes/cartao-postagem/cartao-postagem.component.ts
    	modified:   src/app/componentes/usuario-postagem/usuario-postagem.component.html
    	modified:   src/app/componentes/usuario-postagem/usuario-postagem.component.ts
    	modified:   src/app/paginas/lista-postagem/lista-postagem.component.html
    	modified:   src/app/paginas/lista-postagem/lista-postagem.component.ts

--- src/app/componentes/cartao-postagem/cartao-postagem.component.ts
+++ src/app/componentes/cartao-postagem/cartao-postagem.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {Postagem} from '../../models/post.model';
+import {AcoesPostagemComponent} from '../acoes-postagem/acoes-postagem.component';
+import {UsuarioPostagemComponent} from '../usuario-postagem/usuario-postagem.component';
 
 @Component({
   selector: 'app-cartao-postagem',
-  imports: [],
+  imports: [
+    AcoesPostagemComponent,
+    UsuarioPostagemComponent
+  ],
   templateUrl: './cartao-postagem.component.html',
   styleUrl: './cartao-postagem.component.css'
 })
 export class CartaoPostagemComponent {
-
+  postagem = input.required<Postagem>();
 }
