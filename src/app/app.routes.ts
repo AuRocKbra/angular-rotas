6f2c3c87380dc61c135ef7e4b28aba5161e293ad
augusto.espindola <augusto.espindola@sesicni.com.br>
2025-10-02 09:00:23 -0300

    Criando componente de layout e ajustando rotas
    Cria o componente de layout para definir um template de página principal e ajustando rotas para contemplar novo componente
    
    	modified:   src/app/app.routes.ts
    	new file:   src/app/componentes/layout/layout.component.css
    	new file:   src/app/componentes/layout/layout.component.html
    	new file:   src/app/componentes/layout/layout.component.spec.ts
    	new file:   src/app/componentes/layout/layout.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import {RegistroComponent} from './paginas/registro/registro.component';
 import {ListaPostagemComponent} from './paginas/lista-postagem/lista-postagem.component';
 import {AuthGuard} from './guards/auth.guard';
+import {LayoutComponent} from './componentes/layout/layout.component';
 export const routes: Routes = [
   {path: 'login',component: RegistroComponent},
-  {path: 'posts',canActivate:[AuthGuard],component: ListaPostagemComponent},
+  {path: '',canActivate:[AuthGuard],component: LayoutComponent,children:[
+    {path:'posts',component: ListaPostagemComponent},
+    ]},
   {path: '**',component: RegistroComponent}
 ];
